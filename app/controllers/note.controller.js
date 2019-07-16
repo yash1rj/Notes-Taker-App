cd7a46db1bdbe9f01df856be6c35c50717f93913
Yash Raj <yashrj130@gmail.com>
2019-07-16 17:28:46 +0530

    Update note.controller.js

--- app/controllers/note.controller.js
+++ app/controllers/note.controller.js
@@ -2,17 +2,65 @@ const Note = require('../models/note.model.js');
 
 // Create and Save a new Note
 exports.create = (req, res) => {
+    // Validating request
+    if(!res.body.content) {
+        return res.status(400).send({
+            message: "Note content cannot be empty"
+        });
+    }
 
+    // Create a Note
+    const note = new Note({
+        title: req.body.title || "Untitled Note",
+        content: req.body.content
+    });
+
+    // save Note in the database
+    note.save()
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while creating the Note"
+            });
+        });
 };
 
 // Retrieve and return all notes from the database.
 exports.findAll = (req, res) => {
-
+    Note.find()
+        .then(notes => {
+            res.send(notes);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while retrieving Notes"
+            });
+        });
 };
 
 // Find a single note with a noteId
 exports.findOne = (req, res) => {
-
+    Note.findById(req.params.noteId)
+        .then(note => {
+            if(!note) {
+                return res.status(404).send({
+                    message: "Note not found with id " + req.params.noteId
+                });
+            }
+            res.send(note);
+        })
+        .catch(err => {
+            if(err.kind === 'ObjectId') {
+                return res.status(404).send({
+                    message: "Note not found with id " + req.params.noteId
+                });
+            }
+            return res.status(500).send({
+                message: "Error retrieving note with id " + req.params.noteId
+            });
+        });
 };
 
 // Update a note identified by the noteId in the request
